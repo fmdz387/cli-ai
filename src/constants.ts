ebb79f5fd82589a5def29eb73ce261d272728bb0
fmdz387 <fahir@live.com>
2026-01-17 20:28:48 +0100

    v3.1.2

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ import type { AIProvider, AppConfig } from './types/index.js';
 /**
  * Application version
  */
-export const VERSION = '3.1.1';
+export const VERSION = '3.1.2';
 
 /**
  * Application name
